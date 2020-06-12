bc85baab42376672a9e77030ef1c0a992c9ef9af
unLomTrois <vadim.kuz02@gmail.com>
2020-06-12 06:17:25 +0300

    add not-fetch option

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { getVacancies } from './hh/vacancies.ts'
-import { green } from 'https://deno.land/std/fmt/colors.ts';
+import { green, yellow } from 'https://deno.land/std/fmt/colors.ts';
 
 import { ensureDir } from "https://deno.land/std/fs/mod.ts";
 
@@ -37,27 +37,34 @@ const order_by = cli_args.order ?? 'salary_desc';
 **/
 const experience = cli_args.experience ?? 'noExperience'
 
+//* not fetch *//
+const not_fetch: boolean = cli_args.n ?? false; // -n
 
 /// FETCH PART
-const data = await getVacancies({
-  baseURL: 'https://api.hh.ru',
-  method: '/vacancies',
-  query: {
-    '?no_magic': true,
-    'area': area,
-    'per_page': 100,
-    'page': 0,
-    'order_by': order_by,
-    'text': text,
-    'experience': experience
-  }
-}, {
-  'User-Agent': 'deno_hh_parser/0.2.0'
-}, limit, avoid_words);
+
+if (!not_fetch) {
+  const data = await getVacancies({
+    baseURL: 'https://api.hh.ru',
+    method: '/vacancies',
+    query: {
+      '?no_magic': true,
+      'area': area,
+      'per_page': 100,
+      'page': 0,
+      'order_by': order_by,
+      'text': text,
+      'experience': experience
+    }
+  }, {
+    'User-Agent': 'deno_hh_parser/0.2.0'
+  }, limit, avoid_words);
 
 
-/// SAVE PART
-await ensureDir("./log");
+  /// SAVE PART
+  await ensureDir("./log");
 
-await Deno.writeFile("./log/vacancies.json", new TextEncoder().encode(JSON.stringify(data, undefined, 2)));
-console.log(green('vacancies.json have been saved'));
+  await Deno.writeFile("./log/vacancies.json", new TextEncoder().encode(JSON.stringify(data, undefined, 2)));
+  console.log(green('vacancies.json have been saved'));
+} else {
+  console.log(yellow('NOT FETCH'))
+}
