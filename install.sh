3bc783eccaefa68375604d01fd0aec85a35ad22f
unLomTrois <vadim.kuz02@gmail.com>
2020-06-21 14:35:04 +0300

    add bundling

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-deno install -f --allow-net=api.hh.ru --allow-write=./log --allow-read --unstable -n deno-hh-parser ./src/index.ts
+deno install -f --allow-net=api.hh.ru --allow-write=./log --allow-read --unstable -n deno-hh-parser ./deno-hh-parser.bundle.js
